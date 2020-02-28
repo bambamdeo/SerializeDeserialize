436614996c5f3eb1c170c4aa74f99090c9267fa0
Bambam Deo <11490008+bambamdeo@users.noreply.github.com>
2020-02-28 20:22:43 +0530

    Add files via upload

---
+++ XsltToHtml/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace XsltToHtml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strPath = Directory.GetCurrentDirectory() + @"\Students.xml";
+            var strInput = File.ReadAllText(strPath);
+
+
+
+            Console.WriteLine(strInput);
+            var html = GetHtml(strInput);
+            Console.WriteLine(html);
+
+            Console.ReadLine();
+            Console.ReadLine();
+
+        }
+
+        public static string GetHtml(string xml)
+        {
+            XslCompiledTransform objXSLTransform = new XslCompiledTransform();
+            StringBuilder htmlOutput = new StringBuilder();
+            try
+            {
+                var strXSLTFile = Directory.GetCurrentDirectory() + @"\Students.xslt";
+
+                var strXMLFile = Directory.GetCurrentDirectory() + @"\Students.xml";
+                XmlReader reader = XmlReader.Create(strXMLFile);
+
+              
+                objXSLTransform.Load(strXSLTFile);
+               
+                TextWriter htmlWriter = new StringWriter(htmlOutput);
+                objXSLTransform.Transform(reader, null, htmlWriter);
+
+              
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            #region Load dat from xml string
+            try
+            {
+                StringBuilder htmlOutput2 = new StringBuilder();
+                TextWriter htmlWriter2 = new StringWriter(htmlOutput2);
+                TextReader sr = new StringReader(xml);
+
+                XmlReader reader = XmlReader.Create(sr);
+
+
+                objXSLTransform.Transform(reader, null, htmlWriter2);
+
+                Console.WriteLine(htmlOutput2.ToString());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            #endregion
+
+            return htmlOutput.ToString();
+        }
+    }
+}
---
+++ XsltToHtml/Students.xslt
@@ -0,0 +1,38 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl">
+  <xsl:output method="html" indent="yes"/>
+
+  <xsl:template match="/">
+    <html>
+      <head></head>
+      <body>
+        <xsl:for-each select="//Student">
+          <div style="border:1px black solid;width:auto;margin:1px">
+
+            <div style="display:inline-block;">
+              <b>Student ID:</b>
+              <xsl:value-of select="ID"/>
+            </div>
+            <div style="display:inline-block;">
+              <b>Name:</b>
+              <xsl:value-of select="Name"/>
+              <xsl:text> </xsl:text>
+              <xsl:value-of select="Surname"/>
+            </div>
+            <div style="display:inline-block;">
+              <b>Course:</b>
+              <xsl:value-of select="Course"/>
+            </div>
+            <div style="display:inline-block;">
+              <b>Age:</b>
+              <xsl:value-of select="Age"/>
+            </div>
+          </div>
+        </xsl:for-each>
+      </body>
+    </html>
+  </xsl:template>
+
+
+</xsl:stylesheet>
